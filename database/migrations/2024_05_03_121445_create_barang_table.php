5ac355580dd63e8a6e69b8dc3198eb9e137992e2
shofilabib21@gmail.com <shofilabib21@gmail.com>
2024-05-03 19:20:25 +0700

    first commit

---
+++ database/migrations/2024_05_03_121445_create_barang_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('barang', function (Blueprint $table) {
+            $table->id('kodebarang');
+            $table->string('namabarang');
+            $table->integer('hargsbarang');
+            $table->string('deskripsi');
+            $table->id('kodebarang');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('barang');
+    }
+};
